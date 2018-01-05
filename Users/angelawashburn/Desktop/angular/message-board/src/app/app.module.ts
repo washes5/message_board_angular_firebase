9aed6628d47496f1cc2e747aae973ae685f6f5bf
Angie <angiewashburn1997@gmail.com>
2018-01-05 17:35:31 -0500

    module 2

--- Users/angelawashburn/Desktop/angular/message-board/src/app/app.module.ts
+++ Users/angelawashburn/Desktop/angular/message-board/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: '**', redirectTo: '/'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
