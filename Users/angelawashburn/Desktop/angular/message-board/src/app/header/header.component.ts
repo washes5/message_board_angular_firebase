dc89b1a40e125b1a6476283626c74f1138d0fb44
Angie <angiewashburn1997@gmail.com>
2018-01-05 20:16:42 -0500

    step 6 module 3

--- Users/angelawashburn/Desktop/angular/message-board/src/app/header/header.component.ts
+++ Users/angelawashburn/Desktop/angular/message-board/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  nameToDisplay = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  login() {
+    //Hey, the login function ran!
+    //Set the 'nameToDisplay' variable equal to the answer from a prompt popup
+    this.nameToDisplay = window.prompt('Please enter your name:');
+  }
+
+  logout() {
+    //Hey, the logout function ran!
+    //Clear the 'nameToDisplay' variable.
+    this.nameToDisplay = '';
+  }
 }
